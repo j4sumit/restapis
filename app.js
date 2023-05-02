ad5fb5f7df3162470972dabc170ded0eb876c7c4
unknown <J4sumit@gmail.com>
2023-05-02 18:31:43 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const PORT =process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am live");
+});
+
+const start = async() => {
+    try{
+        app.listen(PORT, () => {
+       console.log(`${PORT} yes i am connected`);
+        });
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+};
+
+start();
